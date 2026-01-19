e7e993b181d4da60f8f108027107518eee183753
poramental <poramental@mail.ru>
2026-01-19 06:21:02 +0300

    feat: add eureka server

---
+++ eurekaserver/src/test/java/com/vsu_schedule/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsu_schedule.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
