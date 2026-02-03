ed930753b5df636caf5a14be30ceeae71913c426
DeathCrushZzZ <Rustforthefuture@yandex.ru>
2026-02-03 22:22:43 +0300

    chore(migrations): seed dev admin user

---
+++ schedule-service/src/main/resources/db/migration/V1_0_2__seed_admin_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO "app_user" (user_id, created_at, login, lastname, name, email, password, status)
+VALUES ('00000000-0000-0000-0000-000000000001', CURRENT_DATE, 'admin', 'Admin', 'Admin', 'admin@example.com',
+        '$2b$05$zscMGvLH8CLtiFrg/MaxXudWOK0oWbIVP9aDUoBb4DgIVwzCjtJrm', 'ACTIVE');
